3ca9c42bab555a9c452b06dfe73f932314564092
chiarabuck <chiars>
2022-07-14 11:30:22 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,8 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class QueryProcessor {
 
@@ -12,9 +14,22 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "Chiara";
+        } else if (query.contains("which of the following numbers is the largest:")){
+            String[] array = query.split(",");
+            int max = Integer.parseInt(array[0]);
+            for(int i = 1; i < array.length; i++ )
+                if (Integer.parseInt(array[i]) > max)
+                    max = Integer.parseInt(array[i]);
+            return String.valueOf(max);
+        } else if (query.contains("what is")){ ;
+            String[] array = query.split(" ");
+            return Integer.parseInt(array[3]) + Integer.parseInt(array[5]) + "";
         } else { // TODO extend the programm here
             return "";
         }
     }
 }
+
+
+
